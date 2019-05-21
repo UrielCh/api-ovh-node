6b6e385a7f01ea2ae135e7dd0a8794128df4dfc9
urielCh <uriel.chemouni@gmail.com>
2019-05-21 10:02:38 +0300

    refactor total

---
+++ code/common/README.md
@@ -0,0 +1,10 @@
+# @ovh-api/common
+
+You should not import this package directly
+
+Use a package like
+* @ovh-api/domain
+* @ovh-api/vps
+* @ovh-api/ip
+* @ovh-api/order
+* ...
---
+++ code/common/index.ts
@@ -0,0 +1,49 @@
+/**
+ * all type that should be sent as parameter to Ovh calls
+ */
+export type OvhParamType = { [key:string]: string | number | undefined | boolean | string[] | boolean[] | number[]; };
+
+/**
+ * contains `requestPromised`
+ */
+export interface OvhRequestable {
+   /**
+     * Execute a request on the API with promise
+     *
+     * @param httpMethod: The HTTP method GET POST PUT DELETE
+     * @param path: The request path with {pathParams}
+     * @param params: The request parameters (passed as query string or body params)
+     */
+    requestPromised(httpMethod: string, path: string, params?: OvhParamType) : Promise<any>;
+}
+
+/**
+ * Parent class of all Ovh Api helper
+ */
+export class OvhWrapper {
+    /**
+     * The Ovh Raw engine
+     * can be the official ovh api or the new Typescript engine
+     * ovh must implements `requestPromised`
+     */
+    private ovh: OvhRequestable;
+    constructor(ovh: OvhRequestable) {
+        this.ovh = ovh;
+    }
+    
+    protected get(path: string, params?: OvhParamType): Promise<any> {
+        return this.ovh.requestPromised('GET', path, params)
+    }
+
+    protected put(path: string, params?: OvhParamType): Promise<any> {
+        return this.ovh.requestPromised('PUT', path, params)
+    }
+
+    protected post(path: string, params?: OvhParamType): Promise<any> {
+        return this.ovh.requestPromised('POST', path, params)
+    }
+
+    protected delete(path: string, params?: OvhParamType): Promise<any> {
+        return this.ovh.requestPromised('DELETE', path, params)
+    }
+}
