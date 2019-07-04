b6ba4304f0f39e1dd00e5cd904080af48afae5ba
urielCh <uriel.chemouni@gmail.com>
2019-07-04 19:16:27 +0300

    start standard imp

--- code/gen/src/index.ts
+++ code/gen/src/index.ts
@@ -1,8 +1,8 @@
-import { Parameter } from './schema';
 import fse from 'fs-extra'
 import Path from 'path'
-import GenApiTypes, { CacheApi, CacheModel, filterReservedKw } from './GenApiTypes';
+import GenApiTypes from './GenApiTypes';
 import { CodeGenerator } from './CodeGenerator';
+import Promise from 'bluebird';
 
 async function main() {
     let gen = new GenApiTypes();
@@ -11,27 +11,28 @@ async function main() {
     // await fse.mkdir('dist');
 
     // debug gen a single API
-    if (true)
+    if (false)
         apis = apis.filter((api) => {
             //if ('/price' == api)
             //    return true;
-            if ('/order' == api)
+            //if ('/order' == api)
+            //    return true;
+            if ('/cdn/webstorage' == api)
                 return true;
             //if ('/distribution/image' == api)
             //    return true;
             return false;
         })
     fse.ensureDirSync('dist');
-    for (const api of apis) {
+
+    await Promise.map(apis, async api => {
         let flat = api.replace(/\//g, '');
         let fn = `dist${Path.sep}${flat}.ts`;
         let cg = new CodeGenerator(api);
-        //extraNS = `OVH${flat}.`;
-        // extraNS = `OVH.`;
         let code = await cg.generate()
         console.log(`saving ${fn}`);
         await fse.writeFile(fn, code);
-    }
+    }, { concurrency: 4 });
 }
 
 main().then(() => console.log);
