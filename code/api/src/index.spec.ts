fb079c34ac78cddbed070fc3d96736a4834c1e0e
urielCh <uriel.chemouni@gmail.com>
2019-07-08 14:26:26 +0300

    update test

--- code/api/src/index.spec.ts
+++ code/api/src/index.spec.ts
@@ -10,25 +10,25 @@ const ovhEngine = new Ovh(config);
 const api = ApiMe(ovhEngine);
 const proxy = proxyMe(ovhEngine);
 
-describe('OvhMe', () => {
-  it('retrive nichandler', async () => {
-    //ovhEngine.addListener()
-    let lastEv: { method: string, path: string, pathTemplate: string } | null = null;
-    let catchReq = (ev: { method: string, path: string, pathTemplate: string }) => {
-      let { method, path, pathTemplate } = ev;
-      if (~(<string>path).indexOf('/auth/'))
-        return;
-      lastEv = ev; 
-      ovhEngine.off('request', catchReq);
-    };
+let lastEv: { method: string, path: string, pathTemplate: string } | null = null;
+let catchReq = (ev: { method: string, path: string, pathTemplate: string }) => {
+  let { method, path, pathTemplate } = ev;
+  if (~(<string>path).indexOf('/auth/'))
+    return;
+  lastEv = ev; 
+  ovhEngine.off('request', catchReq);
+};
 
+describe('OvhMe', () => {
+  it('retrive nichandler Flat call', async () => {
     ovhEngine.on('request', catchReq);
     let constact = await api.get('/me')();
     assert.match(constact.nichandle, /[a-z]{2}[0-9]+-ovh/, 'have nichandle');
     assert.deepEqual(lastEv, { method: 'GET', path: '/me', pathTemplate: '/me' }, 'last Event /me flat call')
-
+  }).timeout(60000);
+  it('retrive nichandler proxy call', async () => {
     ovhEngine.on('request', catchReq);
-    let constact2 = await proxy.$get();// (<Me>test).$get();
+    let constact = await proxy.$get();
     assert.match(constact.nichandle, /[a-z]{2}[0-9]+-ovh/, 'have nichandle proxy');
     assert.deepEqual(lastEv, { method: 'GET', path: '/me', pathTemplate: '/me' }, 'last Event /me dyna call')
   }).timeout(60000);
