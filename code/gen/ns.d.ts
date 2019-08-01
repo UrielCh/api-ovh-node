6559af212849f5baf690ab94a8213a93b25d2e64
urielCh <uriel.chemouni@gmail.com>
2019-08-01 08:25:38 +0300

    update dependences

---
+++ code/gen/ns.d.ts
@@ -0,0 +1,13 @@
+declare type AA = I0;
+export declare namespace B {
+    interface I1 extends AA {
+        f0: string;
+    }
+    interface I0 {
+        f0: number;
+    }
+}
+export interface I0 {
+    f0: string;
+}
+export {};
