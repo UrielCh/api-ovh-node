867072d3671029570030177605f3048f083be053
urielCh <uriel.chemouni@gmail.com>
2019-10-04 13:40:13 +0300

    replace debounce by handcrafted one V 1.2.8

--- samples/ovh-telephony-events/src/main.ts
+++ samples/ovh-telephony-events/src/main.ts
@@ -4,8 +4,6 @@ import { createHandyClient, IHandyRedis } from 'handy-redis';
 import { OvhEventListenerV1 } from './OvhEventListenerV1';
 import { OvhEventListenerV2 } from './OvhEventListenerV2';
 import { OvhEventTokenImporter } from './OvhEventTokenImporter';
-// import { debounce } from 'debounce';
-import { throttle, debounce } from 'throttle-debounce';
 
 // sample exec line:
 // ts-node main.ts --redis-host 127.0.0.1 --cache tokens.json --channel event-voip
@@ -16,8 +14,24 @@ program
     .option('--redis-password <password>', 'provide a redis password')
     .option('--channel <channel>', 'channel key used in redis to push events')
     .option('--cache <cacheFile.json>', 'store and cache tokens')
+    .option('--debounce <3000>', 'debounce log in ms', /[0-9]+/, "3000")
     .option('--v1', 'use Api V1 (by default use V2)')
     .parse(process.argv);
+
+export function myDebounce(
+    delay: number,
+    callback: () => any,
+): () => any {
+    let next: NodeJS.Timeout | null = null;
+    return () => {
+        if (next)
+            return;
+        next = setTimeout(() => {
+            callback();
+            next = null;
+        }, delay)
+    }
+}
 
 async function main() {
     let redis: IHandyRedis | null = null;
@@ -33,34 +47,31 @@ async function main() {
     else
         listener = new OvhEventListenerV2(tokens);
 
-
     let channel = program.channel || 'event-voip';
 
     if (redis)
         listener.redis(redis, channel)
 
-    let fromtime = 0;
+    // let fromtime = 0;
     let nbEvent = 0;
 
     const log = () => {
         const now = new Date();
-        const duration = (now.getTime() - fromtime) / 1000;
-        console.log(`${now.toISOString()} Send ${nbEvent} event to ${channel} in ${duration.toFixed(1)} s`);
+        console.log(`${now.toISOString()} Send ${nbEvent} event to ${channel}`);
         nbEvent = 0;
+        // fromtime = 0;
     };
-    //const logEvents = debounce(log, 500, true);
-    //const logEvents = throttle(3000, log, true)
-    const logEvents = debounce(5000, false, log)
+    const logEvents = myDebounce(program.debounce, log)
     listener.on("message", (ev) => {
+        nbEvent++;
+        // if (!fromtime)
+        //    fromtime = new Date().getTime();
         if (!redis) {
             console.log(ev);
-        } else {
-            if (!nbEvent)
-                fromtime = new Date().getTime();
-            nbEvent++;    
-            logEvents();
         }
+        logEvents();
     })
     await listener.listen();
+    return 'ready';
 }
 main().then(console.log)
