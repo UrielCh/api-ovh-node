ed6760beaae57e450fd1f21fd671f20d1c707de4
urielCh <uriel.chemouni@gmail.com>
2019-11-20 21:43:09 +0200

    updat sample me

--- samples/ovh-me/index.ts
+++ samples/ovh-me/index.ts
@@ -1,11 +1,16 @@
 import Ovh from '@ovh-api/api';
 import ApiMe from '@ovh-api/me';
 
+
 // Create your first application tokens here: https://api.ovh.com/createToken/?GET=/me
-const config = { appKey: String(process.env.APP_KEY),
-    appSecret: String(process.env.APP_SECRET),
-    consumerKey: String(process.env.CONSUMER_KEY)
-};
+let config = {};
+if (process.env.APP_KEY && process.env.APP_SECRET && process.env.CONSUMER_KEY)
+    config = {
+        appKey: String(process.env.APP_KEY),
+        appSecret: String(process.env.APP_SECRET),
+        consumerKey: String(process.env.CONSUMER_KEY)
+    };
+
 const ovh = new Ovh(config);
 const apiMe = ApiMe(ovh);
 
