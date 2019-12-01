689a29732c019e342426be41cd1eac315e1a81ee
urielCh <uriel.chemouni@gmail.com>
2019-12-01 20:01:04 +0200

    update cleaner script

--- samples/ovh-cleaner/index.ts
+++ samples/ovh-cleaner/index.ts
@@ -1,19 +1,22 @@
-import ApiWeb, { proxyHostingWeb, hosting } from '@ovh-api/hosting-web'
-import ApiDom, { proxyDomain }from '@ovh-api/domain'
-import ApiMe, { proxyMe, api } from '@ovh-api/me'
+import ApiWeb, { hosting } from '@ovh-api/hosting-web'
+import ApiDom from '@ovh-api/domain'
+import ApiMe, { api } from '@ovh-api/me'
 import Promise from 'bluebird'
 import dns from 'dns';
 import Ovh from '@ovh-api/api';
 
-const ovh = new Ovh({ accessRules: 'GET /hosting/web, GET /hosting/web/*, GET /domain, POST /hosting/web/*/terminate, GET /me/api*, DELETE /me/api*', certCache: '../../cleanToken.json' });
-const apiWeb = proxyHostingWeb(ovh);
-const apiDom = proxyDomain(ovh);
-const apiMe = proxyMe(ovh);
+
+''
+
+const ovh = new Ovh({ accessRules: 'GET /hosting/web, GET /hosting/web/*, GET /domain, POST /hosting/web/*/terminate, GET /me/api*, DELETE /me/api*, DELETE /hosting/web/*', certCache: '../../cleanToken2.json' });
+const apiWeb = ApiWeb(ovh);
+const apiDom = ApiDom(ovh);
+const apiMe = ApiMe(ovh);
 
 let resolve4 = Promise.promisify(dns.resolve4);
 
 async function cleanApiCert() {
-    let credencials: api.Credential[] = []; 
+    let credencials: api.Credential[] = [];
     let applicationMap = new Map<Number, api.Application>();
     try {
         let cnt = 0;
@@ -23,7 +26,7 @@ async function cleanApiCert() {
         console.log(`Found ${total} credentials\n`)
         await Promise.map(certIds, async (credentialId) => {
             let credencial = await apiMe.api.credential.$(credentialId).$get();
-            let {expiration} = credencial;
+            let { expiration } = credencial;
 
             if (expiration && new Date(expiration).getTime() < now) {
                 console.log(new Date(expiration).getTime() < now);
@@ -102,5 +105,81 @@ async function cleanDomain() {
     }
 }
 
+
+async function cleanMultiSite() {
+    try {
+        let cnt = 0;
+        let doms = await apiDom.$get()
+        let domSet = new Set(doms);
+        console.log(`${doms.length} Domaines`)
+        let hostings = await apiWeb.web.$get()
+        console.log(`${hostings.length} hostings`)
+        await Promise.map(hostings, async (serviceName: string) => {
+            try {
+                let attached = await apiWeb.web.$(serviceName).attachedDomain.$get();
+                const total = attached.length;
+                attached = attached.filter(dom => {
+                    let i = dom.lastIndexOf('.');
+                    i = dom.lastIndexOf('.', i - 1);
+                    if (i >= 0)
+                        dom = dom.substring(i + 1)
+                    if (dom === 'ovh.net')
+                        return false;
+                    if (dom == serviceName)
+                        return false;
+                    if (domSet.has(dom))
+                        return false;
+                    return true;
+                });
+
+                if (!attached.length)
+                    return;
+                const atts = await Promise.map(attached, async (domain) => {
+                    let resolved = ['0.0.0.0'] as any;
+                    try {
+                        resolved = await resolve4(domain);
+                        //if (!resolved.length)
+                        //    resolved = ['0.0.0.0'] 
+                    } catch (e) {
+                    }
+                    return {
+                        domain, ip: resolved.join(', ')
+                    }
+                }, { concurrency: 5 })
+                //console.log(atts.map(({ domain, ip }) => `http://${domain}/ ${ip}`).join('\n'));
+                const toDelete = atts.filter(({ ip }) => ip === '0.0.0.0');
+                console.log(`${serviceName} has ${total} attached domain, ${attached.length} may be removed ${toDelete.length} to remove`); // : ${attached.join(",")}
+                if (toDelete.length == 0)
+                    return;
+                console.log(`${serviceName} ${toDelete.length} toDelete:`)//, toDelete.map(({ domain, ip }) => `http://${domain}/ ${ip}`).join('\n'))
+                await Promise.map(toDelete, async ({ domain }) => {
+                    let time = 0;
+                    try {
+                        // console.log(`start removeing ${serviceName} attachedDomain ${domain}`);
+                        let task = await apiWeb.web.$(serviceName).attachedDomain.$(domain).$delete();
+                        const { id } = task;
+                        while (task.status != "done") {
+                            await Promise.delay(1000);
+                            time++;
+                            task = await apiWeb.web.$(serviceName).tasks.$(id).$get();;
+                        }
+                    } catch (e3) {
+                        if (time > 0)
+                            console.log(`${++cnt} ${serviceName}/${domain} removed after ${time} loop`);
+                        else
+                            console.log(e3);
+                    }
+                }, { concurrency: 8 });
+            } catch (e) {
+                // console.log(serviceName, e)
+            }
+        }, { concurrency: 10 })
+    } catch (ERROR) {
+        console.log(ERROR)
+    }
+}
+
+
 // cleanApiCert().then(() => console.log('fini'));
-cleanDomain().then(() => console.log('fini'));
+// cleanDomain().then(() => console.log('fini'));
+cleanMultiSite().then(() => console.log('fini'));
