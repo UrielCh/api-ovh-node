5eeff1a7c472d95fd5a9ed4665de074638996ede
urielCh <uriel.chemouni@gmail.com>
2019-12-31 20:48:02 +0200

    update ov-delete-service

--- samples/ovh-delete-service/src/index.ts
+++ samples/ovh-delete-service/src/index.ts
@@ -4,20 +4,42 @@ import program from 'commander'
 import Bluebird from 'bluebird';
 import fse from 'fs-extra';
 
+// ts-node src/index.ts -d delete.txt -k keep.txt
+
+
 const { version } = require('../package.json');
 
 program
   .version(version)
-  .parse(process.argv)
+  .option('--cancel', "cancel resiliation")
+  .option('-d, --delete <file>', "file containing services to delete")
+  .option('-k, --keep <file>', "file containing services to keep")
+  .parse(process.argv);
 
 async function main() {
-  let todelete = fse.readFileSync('delete.txt', { encoding: 'utf-8' })
+  if (!program.delete) {
+    console.log('delete source file must be provided');
+    return;
+  }
+  let toDelete = fse.readFileSync(program.delete, { encoding: 'utf-8' })
     .split(/[\r\n]+/)
     .map((s: string) => s.replace(/ /g, ''))
     .map((s: string) => s.replace(/^0([0-9]{9})$/, '0033$1'));
+  console.log('toDelete:' + toDelete.length);
 
-  console.log('todelete:' + todelete.length);
-
+  if (program.keep) {
+    let toKeep = fse.readFileSync(program.keep, { encoding: 'utf-8' })
+      .split(/[\r\n]+/)
+      .map((s: string) => s.replace(/ /g, ''))
+      .map((s: string) => s.replace(/^\+./, '00'))
+      .map((s: string) => s.replace(/^0([0-9]{9})$/, '0033$1'));
+    let toKeepSet = new Set(toKeep);
+    console.log('toKeep:' + toKeep.length);
+    toDelete = toDelete.filter(f => !toKeepSet.has(f));
+    console.log('filtred toDelete:' + toDelete.length);
+  }
+  if (program.cancel)
+    console.log('All matching termination will be canceled');
   let param: OvhParams = { accessRules: `GET /telephony, GET /telephony/*, DELETE /telephony/*`, certCache: '../../token2.json' };
   console.log('cert Ok');
   let ovh = new Ovh(param)
@@ -27,22 +49,32 @@ async function main() {
   const billingAccounts = await apiPhone.$get();
   console.log('billingAccounts:', billingAccounts);
   let services = {} as { [key: string]: { billingAccount: string, type: 'number' | 'line', name: string } };
-
+  let [nbLine, nbNumber] = [0, 0]
   await Bluebird.map(billingAccounts, async (billingAccount: string) => {
     try {
-      console.log('group: ' + billingAccount)
       const lines = await apiPhone.$(billingAccount).line.$get();
       const numbers = await apiPhone.$(billingAccount).number.$get();
+      nbLine += lines.length;
+      nbNumber += numbers.length;
       lines.forEach((name: string) => services[name] = { billingAccount, type: 'line' as 'line', name });
       numbers.forEach((name: string) => services[name] = { billingAccount, type: 'number' as 'number', name });
+      if (lines.length == 0 && numbers.length == 0)
+        return;
+      let msg = `group: ${billingAccount} contains`;
+      if (numbers.length)
+        msg += ` ${numbers.length} numbers`;
+      if (lines.length)
+        msg += ` ${lines.length} lines`;
+      console.log(msg);
     } catch (e) {
-      console.log(`billing: ${billingAccount}: ${e}`);
+      if (e !== '404: This service does not exist')
+        console.log(`billing: ${billingAccount}: ${e}`);
       return;
     }
   }, { concurrency: 8 });
-
+  console.log(`total Line:${nbLine} total Number:${nbNumber}`);
   let match = 0;
-  await Bluebird.map(todelete, async (servicename: string) => {
+  await Bluebird.map(toDelete, async (servicename: string) => {
     const info = services[servicename];
     if (!info) {
       // console.log(`no Service ${servicename}`);
@@ -50,8 +82,13 @@ async function main() {
     }
     match++;
     try {
-      console.log(`deleting Service ${info.billingAccount}/${servicename}`);
-      await apiPhone.$(info.billingAccount).service.$(servicename).$delete({ reason: 'addresseMove' })
+      if (program.cancel) {
+        console.log(`cancelTermination Service ${info.billingAccount}/${servicename}`);
+        await apiPhone.$(info.billingAccount).service.$(servicename).cancelTermination.$post();
+      } else {
+        console.log(`deleting Service ${info.billingAccount}/${servicename}`);
+        await apiPhone.$(info.billingAccount).service.$(servicename).$delete({ reason: 'addresseMove' })
+      }
     } catch (e) {
       // console.log(e);
     }
