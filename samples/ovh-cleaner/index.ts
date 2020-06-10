10f8c479a30cb382656d422d4a7b2de81975d8d4
urielch <uriel.chemouni@gmail.com>
2020-06-10 14:30:48 +0300

    update ovh-cleaner

--- samples/ovh-cleaner/index.ts
+++ samples/ovh-cleaner/index.ts
@@ -1,41 +1,44 @@
-import ApiWeb, { hosting } from '@ovh-api/hosting-web'
-import ApiDom from '@ovh-api/domain'
-import ApiMe, { api } from '@ovh-api/me'
-import Promise from 'bluebird'
+import ApiWeb, { hosting } from '@ovh-api/hosting-web';
+import ApiDom from '@ovh-api/domain';
+import ApiMe, { api } from '@ovh-api/me';
+import Bluebird from 'bluebird';
 import dns from 'dns';
 import Ovh from '@ovh-api/api';
+import program from 'commander';
 
 const ovh = new Ovh({ accessRules: 'GET /hosting/web, GET /hosting/web/*, GET /domain, POST /hosting/web/*/terminate, GET /me/api*, DELETE /me/api*, DELETE /hosting/web/*', certCache: '../../cleanToken2.json' });
-const apiWeb = ApiWeb(ovh);
-const apiDom = ApiDom(ovh);
-const apiMe = ApiMe(ovh);
+const api= {
+    web: ApiWeb(ovh),
+    domain: ApiDom(ovh),
+    me: ApiMe(ovh),
+}
 
-let resolve4 = Promise.promisify(dns.resolve4);
+let resolve4 = Bluebird.promisify(dns.resolve4);
 
 async function cleanApiCert() {
     let credencials: api.Credential[] = [];
     let applicationMap = new Map<Number, api.Application>();
     try {
         let cnt = 0;
-        let certIds = await apiMe.api.credential.$get() // , {status: 'validated'}
+        let certIds = await api.me.api.credential.$get() // , {status: 'validated'}
         let total = certIds.length;
         let now = new Date().getTime();
         console.log(`Found ${total} credentials\n`)
-        await Promise.map(certIds, async (credentialId) => {
-            let credencial = await apiMe.api.credential.$(credentialId).$get();
+        await Bluebird.map(certIds, async (credentialId) => {
+            let credencial = await api.me.api.credential.$(credentialId).$get();
             let { expiration } = credencial;
 
             if (expiration && new Date(expiration).getTime() < now) {
                 console.log(new Date(expiration).getTime() < now);
                 console.log(`${++cnt}/${total} delete Old Cert ${credentialId} expired since ${credencial.expiration}`);
-                await apiMe.api.credential.$(credentialId).$delete();// ('/me/api/credential/{credentialId}', { credentialId });
+                await api.me.api.credential.$(credentialId).$delete();// ('/me/api/credential/{credentialId}', { credentialId });
             }
 
             total--
             let { applicationId } = credencial;
             if (!applicationMap.has(applicationId)) {
                 try {
-                    const app = await apiMe.api.application.$(applicationId).$get();
+                    const app = await api.me.api.application.$(applicationId).$get();
                     applicationMap.set(applicationId, app);
                 } catch (err) {
                     if (String(err).indexOf('does not exist')) {
@@ -72,16 +75,16 @@ App: ${app.name} Desc:${app.description}`);
 async function cleanDomain() {
     try {
         let cnt = 0;
-        let doms = await apiDom.$get()
+        let doms = await api.domain.$get()
         console.log(`${doms.length} Domaines`)
-        let hostings = await apiWeb.web.$get()
+        let hostings = await api.web.web.$get()
         console.log(`${hostings.length} hostings`)
         let hdoms = new Set(doms);
         let total = hostings.length;
         hostings = hostings.filter(h => !hdoms.has(h));
         console.log(`${hostings.length}/${total} dosting have no matching domaines`);
-        await Promise.map(hostings, async (serviceName: string) => {
-            let hosting: hosting.web.Service = await apiWeb.web.$(serviceName).$get();
+        await Bluebird.map(hostings, async (serviceName: string) => {
+            let hosting: hosting.web.Service = await api.web.web.$(serviceName).$get();
             let resolved;
             if (hosting.offer != 'start10m')
                 return;
@@ -90,7 +93,7 @@ async function cleanDomain() {
                 console.log(++cnt + ') KEEP ' + serviceName + ' resolved:' + resolved);
             } catch (e) {
                 try {
-                    await apiWeb.web.$(serviceName).terminate.$post();
+                    await api.web.web.$(serviceName).terminate.$post();
                     console.log(++cnt + ') DROP ' + serviceName + " success");
                 } catch (e1) {
                     console.log(++cnt + ') DROP ' + serviceName + " FAILED " + e1);
@@ -102,21 +105,27 @@ async function cleanDomain() {
     }
 }
 
-async function cleanMultiSite() {
+async function cleanMultiSite(webHosting?: string) {
     try {
         let cnt = 0;
-        const domSet = new Set(await apiDom.$get());
-        console.log(`${domSet.size} Domaines`)
-        let hostings = await apiWeb.web.$get()
-        console.log(`${hostings.length} hostings`)
-        await Promise.map(hostings, async (serviceName: string) => {
+        const domSet = new Set(await api.domain.$get());
+        console.log(`${domSet.size} domaines acvailable in the account`)
+        let hostings = webHosting ? [webHosting]: await api.web.web.$get();
+        console.log(`${hostings.length} hostings will be test`)
+        await Bluebird.map(hostings, async (serviceName: string) => {
             try {
-                const hostingInfo = await apiWeb.web.$(serviceName).$get();
-
+                const hostingInfo = await api.web.web.$(serviceName).$get();
+                if (hostingInfo.offer === 'start10m') {
+                    // skip Start 10M
+                    return;
+                }
                 const validIPs = new Set();
                 validIPs.add(hostingInfo.clusterIp);
                 hostingInfo.countriesIp?.forEach(e => validIPs.add(e.ip));
-                let attached = await apiWeb.web.$(serviceName).attachedDomain.$get();
+                let attached = await api.web.web.$(serviceName).attachedDomain.$get();
+                // console.log(attached.join(', '));
+                let cosite = 0;
+
                 const total = attached.length;
                 attached = attached.filter(dom => {
                     let i = dom.lastIndexOf('.');
@@ -127,14 +136,18 @@ async function cleanMultiSite() {
                         return false;
                     if (dom == serviceName)
                         return false;
-                    if (domSet.has(dom))
+                    if (domSet.has(dom)) {
+                        cosite++;
                         return false;
+                    }
                     return true;
                 });
 
-                if (!attached.length)
+                if (!attached.length) {
+                    console.log(`${serviceName}\t(${hostingInfo.offer})\tcontains ${cosite} attached hosting.`);
                     return;
-                let atts = await Promise.map(attached, async (domain) => {
+                }
+                let atts = await Bluebird.map(attached, async (domain) => {
                     let resolved = ['0.0.0.0'] as any;
                     try {
                         resolved = await resolve4(domain);
@@ -151,37 +164,41 @@ async function cleanMultiSite() {
 
                 const toDelete = atts.filter(({ ips }) => ips[0] === '0.0.0.0');
                 const canDelete = atts.filter(({ ips }) => ips[0] !== '0.0.0.0');
-                if (toDelete.length === 0 && canDelete.length === 0)
-                    return;
-                console.log(`${serviceName} has ${total} attached domain, ${toDelete.length} are pointing to 0.0.0.0 will be removed, and ${canDelete.length} hosting looks not to be used`); // : ${attached.join(",")}
+                if (toDelete.length === 0 && canDelete.length === 0) {
+                    console.log(`${serviceName}\t(${hostingInfo.offer})\tcontains ${cosite + attached.length} attached hosting.`);
+                   return;
+                }
+                console.log(`${serviceName}\t(${hostingInfo.offer})\thas ${total} attached domain, ${toDelete.length} are pointing to 0.0.0.0 will be removed, and ${canDelete.length} hosting looks not to be used by checking they current dns value.`); // : ${attached.join(",")}
                 if (canDelete.length) {
                     console.log(`${serviceName}(${hostingInfo.clusterIp}) ${canDelete.length} not used: ${canDelete.map(({domain, ips}) => `${domain}(${ips.join(',')})`).join(', ')}`)
                     canDelete.forEach(d => toDelete.push(d));
                 }
-                if (toDelete.length == 0)
-                    return;
-                console.log(`${serviceName} ${toDelete.length} toDelete: ${toDelete.map(({domain}) => domain).join(', ')}`)
-                //, toDelete.map(({ domain, ip }) => `http://${domain}/ ${ip}`).join('\n'))
-                await Promise.map(toDelete, async ({ domain }) => {
-                    let time = 0;
-                    let dt = new Date().getTime();
-                    try {
-                        // console.log(`start removeing ${serviceName} attachedDomain ${domain}`);
-                        let task = await apiWeb.web.$(serviceName).attachedDomain.$(domain).$delete();
-                        const { id } = task;
-                        while (task.status != "done") {
-                            await Promise.delay(1000);
-                            time++;
-                            task = await apiWeb.web.$(serviceName).tasks.$(id).$get();;
+                //if (toDelete.length == 0)
+                //    return;
+                if (toDelete.length > 0) {
+                    console.log(`${serviceName} ${toDelete.length} toDelete: ${toDelete.map(({domain}) => domain).join(', ')}`)
+                    //, toDelete.map(({ domain, ip }) => `http://${domain}/ ${ip}`).join('\n'))
+                    await Bluebird.map(toDelete, async ({ domain }) => {
+                        let time = 0;
+                        let dt = new Date().getTime();
+                        try {
+                            // console.log(`start removeing ${serviceName} attachedDomain ${domain}`);
+                            let task = await api.web.web.$(serviceName).attachedDomain.$(domain).$delete();
+                            const { id } = task;
+                            while (task.status != "done") {
+                                await Bluebird.delay(1000);
+                                time++;
+                                task = await api.web.web.$(serviceName).tasks.$(id).$get();;
+                            }
+                        } catch (e3) {
+                            if (time > 0) {
+                                dt = (new Date().getTime() - dt) / 1000;
+                                console.log(`${++cnt}\t${serviceName}/${domain} removed after ${dt.toFixed(1)} Sec`);
+                            } else
+                                console.log(e3);
                         }
-                    } catch (e3) {
-                        if (time > 0) {
-                            dt = (new Date().getTime() - dt) / 1000;
-                            console.log(`${++cnt} ${serviceName}/${domain} removed after ${dt.toFixed(1)} Sec`);
-                        } else
-                            console.log(e3);
-                    }
-                }, { concurrency: 8 });
+                    }, { concurrency: 8 });
+                }
             } catch (e) {
                 // console.log(serviceName, e)
             }
@@ -191,7 +208,15 @@ async function cleanMultiSite() {
     }
 }
 
+program.version('1.0.0');
 
+program.command('cleanMultiSite [webHosting]')
+    .description('delete unused multiSite from webHosting, if non defile, process all web hosting')
+    .action(async (webHosting?: string) => {
+        await cleanMultiSite(webHosting);
+    });
+
+program.parse(process.argv);
 // cleanApiCert().then(() => console.log('fini'));
 // cleanDomain().then(() => console.log('fini'));
-cleanMultiSite().then(() => console.log('fini'));
+
