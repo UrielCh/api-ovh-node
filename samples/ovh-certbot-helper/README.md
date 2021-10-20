4dea9e721c363343ea874f7acbf82f14aec9bea0
UrielCh <uriel.chemouni@gmail.com>
2021-10-20 08:20:25 +0300

    uodate certbot helper

--- samples/ovh-certbot-helper/README.md
+++ samples/ovh-certbot-helper/README.md
@@ -1,9 +1,9 @@
 # ovh-certbot-helper
 
-Just call this script to generate a certbot-dns-ovh's config file, a browser will assist you to create OVH token
+Just call this script to generate a certbot-dns-ovh's config file, a browser will assist you to create OVH token.
 
 ```bash
 npx ovh-certbot-helper
 ```
 
-
+A cli will ask you thw way you want to use certbot (snap or docker)
--- samples/ovh-certbot-helper/index.ts
+++ samples/ovh-certbot-helper/index.ts
@@ -1,9 +1,13 @@
 import OvhEngine from '@ovh-api/api';
 import apiAuth from '@ovh-api/auth';
+import inquirer from 'inquirer';
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
 
 const accessRules = `GET /domain/zone/*, PUT /domain/zone/*, POST /domain/zone/*, DELETE /domain/zone/*`;
 
-async function main() {
+async function getConfigIni(): Promise<string> {
   const ovhEngine = new OvhEngine({ accessRules });
   const auth = apiAuth(ovhEngine);
   const current = await auth.currentCredential.$get();
@@ -14,24 +18,90 @@ dns_ovh_endpoint = ovh-eu
 dns_ovh_application_key = ${ovhEngine.appKey}
 dns_ovh_application_secret = ${ovhEngine.appSecret}
 dns_ovh_consumer_key = ${ovhEngine.consumerKey}`;
+  return config;
+}
+interface Answer {
+  distro: 'docker' | 'snap'
+};
+async function displayGui() {
+  const resp = await inquirer.prompt<Answer>([{ type: 'list', name: 'distro', message: 'which certbot distro do you use ?', choices: ['snap', 'docker'] }])
+  if (resp.distro === 'snap') {
+    console.log('');
+    console.log('sudo snap install certbot-dns-ovh')
+    console.log('sudo certbot certonly --dns-ovh --dns-ovh-credentials ~/.secrets/certbot/ovh.ini --dns-ovh-propagation-seconds 60 -d "*.example.com"')
+  } else if (resp.distro === 'docker') {
+    console.log('');
+    console.log('# add this alias in your .profile and activate it');
+    console.log('alias certbot="docker run -it --rm -v /etc/letsencrypt:/etc/letsencrypt -v ${HOME}/.secrets/certbot/ovh.ini:/ovh.ini certbot/dns-ovh;')
+    console.log('');
+    console.log('sudo mkdir /etc/letsencrypt; sudo chown ${USER}: /etc/letsencrypt;');
+    console.log('certbot certonly --dns-ovh --dns-ovh-credentials /ovh.ini --dns-ovh-propagation-seconds 60 -d "*.example.com"');
+    console.log('');
+    console.log('# renew cert within crontab:');
+    console.log('0 11 * * * . .profile && certbot renew');
+    console.log('');
+    console.log('# You may add an other crontab entry to reload your nginx/apache config to reload your certificate');
+  }
+}
+
+async function createovhIni(dest: string) {
+  const home = os.homedir();
+  let destShort = home;
+  if (dest.startsWith(home)) {
+    destShort = '~' + dest.substring(home.length);
+  }
+  const AUTO = `Let me create the file ${destShort}.`;
+  const SHELL = 'Print a bash conmmands to create the file.';
+  const MANUAL = 'Just print the file.';
+
+  const resp = await inquirer.prompt<{ write: string }>([{
+    type: 'list', name: 'write', message: 'How to process the certificat ?',
+    choices: [AUTO, SHELL, MANUAL]
+  }])
+
+  const config = await getConfigIni();
+
+  if (resp.write === AUTO) {
+    const dir = path.dirname(dest);
+    try {
+      fs.promises.mkdir(dir, { recursive: true });
+    } catch (e) {
+      // ignore
+    }
+    await fs.promises.writeFile(dest, config, {encoding: 'utf8', flag: 'w', mode: 0o600});
+    console.log(`cert generated in ${dest}`);
+  }
+  else if (resp.write === SHELL) {
+    let EOF = 'EOF';
+    if (destShort.includes(EOF)) {
+      EOF = '___EOF___'
+    }
+    console.log('create a new ovh.ini file');
+    console.log(`mkdir -p ${destShort}`);
+    console.log(`cat << ${EOF} > ${destShort}`);
+    console.log(config);
+    console.log(EOF);
+    console.log(`chmod 600 ${destShort}`)
+    console.log('');
+  }
+  else if (resp.write === MANUAL) {
+    console.log('');
+    console.log(config);
+    console.log('');
+  }
+}
+
+async function main() {
+  const dest = path.resolve(os.homedir(), '.secrets', 'ovh.ini');
+  try {
+    await fs.promises.stat(dest)
+    console.log(`${dest} exists on this system, skip generation`);
+  } catch (e) {
+    await createovhIni(dest);
+  }
   console.log('');
-  console.log('Copy this line to a file like ~/.secrets/certbot/ovh.ini');
-  console.log('then use certbot as root ex:')
-  console.log('chmod 600 ~/.secrets/certbot/ovh.ini')
-  console.log('sudo snap install certbot-dns-ovh')
-  console.log('sudo certbot certonly --dns-ovh --dns-ovh-credentials ~/.secrets/certbot/ovh.ini --dns-ovh-propagation-seconds 60 -d "*.example.com"')
+  displayGui()
   console.log('');
-  console.log('using Docker:');
-  console.log('sudo mkdir /etc/letsencrypt');
-  console.log('sudo chown ${USER}: /etc/letsencrypt');
-  console.log('docker run -it --rm --name certbot \
-            -v "/etc/letsencrypt:/etc/letsencrypt" \
-            -v "${HOME}/.secrets/certbot/ovh.ini:/ovh.ini" \
-            certbot/dns-ovh certonly --dns-ovh --dns-ovh-credentials /ovh.ini --dns-ovh-propagation-seconds 60 -d "*.example.com"');  
-  console.log('Docker renew:');
-  console.log('docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "${HOME}/.secrets/certbot/ovh.ini:/ovh.ini" certbot/dns-ovh renew');
-  console.log('');
-  console.log(config);
 }
 
 main();
